5810788bb624fa2381bfd2818656596a73749a23
dwaypro <dangrantway@gmail.com>
2017-06-05 13:12:12 -0700

    Pseudocode

--- homefile.rb
+++ homefile.rb
@@ -4,6 +4,29 @@ Dico accusata percipitur pro ex. At consulatu deseruis3456784se inciderint est, 
 
 Usu error accommodare ad, audire scribentur vis ex, et eos diam etiam nostrum. Quaestio suavitate eu mel, ut vim verear blandit. At nec dicat3456789 voluptaria definitiones, illud labitur consequat ad est, in iuvaret alterum salutatus has. Iusto prompta habemus ad vim. At ius tempor patrioque,2345678 accusata"
 
-content.delete "abcdefghijklmnopqrstuvwxyz"
 
-puts content
+# method that isolates text or scans for numbers and eliminates irrelevant content.
+# method that assigns numbers to a unique variable or stores inside an array or table
+# method that associates two numbers together, or other methods for fun stuff
+
+def number_finder
+
+# loop until end of text or document
+	# get the length of the text or document
+
+# perform task with appropriate methods untill all numbers have been extracted
+	end
+
+def number_bank
+
+# Call this method on the return values of number finder
+# loop until all values in number finder are stored in an array
+# Need two indentifiers: Array reference pair and number_finder result
+
+
+end
+
+# Print to screen the results stored in number_bank
+
+
+
