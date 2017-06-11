484b3b7f02093ebe7632be65ea533f604d3df485
dwaypro <dangrantway@gmail.com>
2017-06-10 20:27:33 -0700

    added feature api

--- homefile.rb
+++ homefile.rb
@@ -5,6 +5,11 @@ require "open-uri"
 require "uri"
 require "nokogiri"
 require "open-uri"
+require 'net/http'
+require 'json'
+require 'darksky'
+require 'httparty'
+
 
 
 content = "Lorem ipsum dolor sit amet, an dicunt noluisse legendos vis, oportere vulputate constituto his ut. Harum quaerendum vis ut. Audiam pertinax sensibus ei sed. Hinc tantas omnesque has at, id has prodesset 1783493 mnesarchum inciderint. Et labore diceret has, at quidam mentitum pri, eu vis eius sensibus oportere.
@@ -14,19 +19,13 @@ Dico accusata percipitur pro ex. At consulatu deseruis3456784se inciderint est, 
 Usu error accommodare ad, audire scribentur vis ex, et eos diam etiam nostrum. Quaestio suavitate eu mel, ut vim verear blandit. At nec dicat3456789 voluptaria definitiones, illud labitur consequat ad est, in iuvaret alterum salutatus has. Iusto prompta habemus ad vim. At ius tempor patrioque,2345678 accusata"
 
 
-def method(data)
+def method_scanner(data)
 
 index = 0
 
 char = data[index]
 
 chararray = []
-
-#Logic to be build:
-#if char is an integer with anything but an integer succeeded it
-#take all integers before and store them into a hash 
-#flag 'first' letter and last 'letter'
-#everything in between goes into hash
 
 
 until index == data.length
@@ -69,7 +68,7 @@ end
 p chararray 	
 end
 
-method(content)
+method_scanner(content)
 
 def htmlmethod(data)
 
@@ -112,7 +111,10 @@ p chararray
 end
 
 
-doc = Nokogiri::HTML(open("http://www.google.com"))
+puts "What content do you want analyzed? do not include http://wwww"
+website_url = gets.chomp
+
+doc = Nokogiri::HTML(open("http://www." + website_url))
 
 htmlcontent = doc.to_html
 
@@ -120,10 +122,52 @@ htmlcontent = doc.to_html
 htmlmethod(htmlcontent)
 
 
-# have control over strings within and outside of application
-# need to include logic that stores information into a referencable hash
-# needs to print pairs 
+
+
+API_KEY = "b01b7f67059eadbe96bf9939334273a8"
+
+darksky = Darksky::API.new('b01b7f67059eadbe96bf9939334273a8')
+
+
+ forecast = darksky.forecast('42.7243', '-73.6927')
+
+#return forecast
+ brief_forecast = darksky.brief_forecast('42.7243','-73.6927')
+
+# returns forecasts at a collection of arbitrary points.
+ precipitation = darksky.precipitation(['42.7','-73.6',1325607100,'42.0','-73.0',1325607791])
+
+ interesting_storms = darksky.interesting
+
+URL = "https://api.forecast.io/forecast/#{ API_KEY }/37.8267,-122.423"
+puts HTTParty.get(URL).body[0, 500]
+
+data_array = []
+body_data = HTTParty.get(URL).body[0, 500]
+data_array << body_data
 
 
 
 
+
+
+def url_method(string_array)
+
+string_array.map! {|x| print x, " hello! "
+
+
+
+}
+
+
+end
+url_method(data_array)
+
+
+
+
+#JSON[returned_json]
+
+#body = open(URL).read
+
+#puts JSON[body]
